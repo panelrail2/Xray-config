bcfbc07eb89fbcb80041baeaa76f9795170b7fb8
panelrail2 <panelrail2@atomicmail.io>
2026-08-06 03:50:05 +0330

    Enhance app.py with JSON responses and health check

--- app/app.py
+++ app/app.py
@@ -1,19 +1,56 @@
-from flask import Flask
+from flask import Flask, jsonify
+import subprocess
 
 
 app = Flask(__name__)
 
 
+
 @app.route("/")
 def home():
 
-    return {
-        "status":"running",
-        "service":"xray"
-    }
+    return jsonify(
+        {
+            "service": "Railway Xray",
+            "status": "running"
+        }
+    )
+
 
 
 @app.route("/health")
 def health():
 
-    return "OK"
+
+    xray = subprocess.run(
+        ["pgrep", "xray"],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL
+    )
+
+
+    nginx = subprocess.run(
+        ["pgrep", "nginx"],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL
+    )
+
+
+
+    if xray.returncode == 0 and nginx.returncode == 0:
+
+        return jsonify(
+            {
+                "status":"ok",
+                "xray":"running",
+                "nginx":"running"
+            }
+        ),200
+
+
+
+    return jsonify(
+        {
+            "status":"failed"
+        }
+    ),503
